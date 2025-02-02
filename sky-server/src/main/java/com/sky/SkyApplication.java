6b25cc7bc6a6751d1787f342213b5d8028afd413
LiuJM <liujiaming6656@gmail.com>
2025-02-02 20:59:26 +0800

    缓存套餐代码实现

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -3,14 +3,16 @@ package com.sky;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
+@EnableTransactionManagement //启用 Spring 的声明式事务管理
+@EnableCaching //启用 Spring 的缓存抽象层
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
-        log.info("server started");
+        log.info("Server started");
     }
 }
--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -7,7 +7,8 @@ import com.sky.service.SetmealService;
 import com.sky.vo.DishItemVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,19 +20,22 @@ import java.util.List;
 @RestController("userSetmealController")
 @RequestMapping("/user/setmeal")
 @Api(tags = "C端-套餐浏览接口")
+@Slf4j
 public class SetmealController {
     @Resource
     private SetmealService setmealService;
 
     /**
-     * 条件查询
+     * 根据分类id查询套餐
      *
      * @param categoryId
      * @return
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询套餐")
+    @Cacheable(cacheNames = "setmaelCache", key = "#categoryId")//key: setmaelCache::categoryId
     public Result<List<Setmeal>> list(Long categoryId) {
+        log.info("根据分类id查询套餐: {}", categoryId);
         Setmeal setmeal = new Setmeal();
         setmeal.setCategoryId(categoryId);
         setmeal.setStatus(StatusConstant.ENABLE);
@@ -49,6 +53,7 @@ public class SetmealController {
     @GetMapping("/dish/{id}")
     @ApiOperation("根据套餐id查询包含的菜品列表")
     public Result<List<DishItemVO>> dishList(@PathVariable("id") Long id) {
+        log.info("根据套餐id查询包含的菜品列表: {}", id);
         List<DishItemVO> list = setmealService.getDishItemById(id);
         return Result.success(list);
     }
