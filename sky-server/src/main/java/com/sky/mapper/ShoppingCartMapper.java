e2dd03f1b45ca4737398d50b6cd1a3e7437aeb1c
LiuJM <liujiaming6656@gmail.com>
2025-02-12 16:08:12 +0800

    订单管理剩余部分代码实现

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -46,4 +46,12 @@ public interface ShoppingCartMapper {
      * @param shoppingCart
      */
     void deleteById(ShoppingCart shoppingCart);
+
+    /**
+     * 批量插入购物车数据
+     *
+     * @param shoppingCartList
+     */
+    void insertBatch(List<ShoppingCart> shoppingCartList);
+
 }
