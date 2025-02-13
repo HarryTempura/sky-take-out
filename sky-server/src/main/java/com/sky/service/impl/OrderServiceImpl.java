7ce99ae401d9bb9a1151f5c571e613eb4f8ed685
LiuJM <liujiaming6656@gmail.com>
2025-02-13 12:01:53 +0800

    来单提醒代码实现

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -21,6 +21,7 @@ import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
@@ -52,6 +53,8 @@ public class OrderServiceImpl implements OrderService {
     private UserMapper userMapper;
     @Resource
     private WeChatPayUtil weChatPayUtil;
+    @Resource
+    private WebSocketServer webSocketServer;
 //    @Value("${sky.shop.address}")
 //    private String shopAddress;
 //    @Value("${sky.baidu.ak}")
@@ -173,6 +176,15 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+
+        //通过websocket向客户端浏览器推送消息
+        Map map = new HashMap();
+        map.put("type", 1);//1表示来单提醒，2表示客户催单
+        map.put("ordersId", ordersDB.getId());
+        map.put("content", "订单号" + outTradeNo);
+
+        String jsonString = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(jsonString);
     }
 
     /**
